d780eb6962d4c783bc1ae3a5af9348f166c802ee
Josué Chaves Araya <d4n11083@estudiantec.cr>
2022-10-12 21:08:44 -0600

    Update programaprincipal.py
    - Funcion para que retorne el path, para no tener que ponerlo a mano cada ves
    -Hice una lista de diccionarios paea guardar las imagenes y mas info para ver que imagen da el "error"
    -Muestra la imagen que tiene 2 dimensiones

--- programaprincipal.py
+++ programaprincipal.py
@@ -13,6 +13,7 @@ from PIL import Image
 import multiprocessing as mp
 import numpy as np
 
+import matplotlib.pyplot as plt
 #Descripción: Serie de prints que muestran las opciones disponibles
 #del menú principal
 #E: Digito
@@ -28,7 +29,6 @@ def menuPrincipal():
     except:
         print("Opción no válida\n")
         return menuPrincipal()
-
     if opcion==1:
         return cargaImagenes()
     if opcion==2:
@@ -39,14 +39,33 @@ def menuPrincipal():
         print("Opción no válida")
         return menuPrincipal()
 
-
+def rutasCarpetaImagenes():
+    print("*****Elija la Ruta ******")
+    print("1. Duan ")
+    print("2. Josue ")
+    print("3. Valen ")
+    try:
+        opcion=int(input("Digite su opcion:"))
+    except:
+        print("Opción no válida\n")
+        return rutasCarpetaImagenes()
+    if opcion==1:
+        return r'C:\Users\Usuario\Desktop\8 semestre\Arquitectura computadores\Proyecto 1\Fotomosaico\PROYECTO1_ARQUI_MULTIPROSSESING\imgs'
+    if opcion==2:
+        return r'/Users/gilda/Desktop/Repositorios/Proyecto_1_Arqui/PROYECTO1_ARQUI_MULTIPROSSESING/imgs'
+    if opcion==3:
+        pass
+    else:
+        print("Opción no válida")
+        return rutasCarpetaImagenes()
 
 #E:
 #S:
 #R:
 def cargaImagenes():
-    ruta=input("Escriba la ruta de la carpeta con las imágenes:")
-    
+    # ruta=input("Escriba la ruta de la carpeta con las imágenes:")
+    ruta = rutasCarpetaImagenes()
+
     if verificarRuta(ruta)==False:
         print("Ruta no válida")
     else:
@@ -56,7 +75,7 @@ def cargaImagenes():
             if os.path.isfile(os.path.join(ruta, fichero)) and fichero.endswith('.jpg'):
                 directorio.append(fichero)
                 
-        print(len(directorio))
+        print(f'Hay {len(directorio)} imagenes en la carpeta')
         
         return cambioTamannoImg(directorio,ruta)
 
@@ -65,47 +84,48 @@ def cargaImagenes():
 #S: Valor booleano
 #R: ...................
 def verificarRuta(ruta):
+    print("------ Verificando Path")
+
     if os.path.exists(ruta):
         return True
     else:
         return False
 
-#
-#
-#
+#Función que cambia el tamaño de las imagenes 
+#E directorio: [] Lista de nombres de las imagenes del folder
+#E ruta: string Path del directorio 
+#S
 def cambioTamannoImg(directorio,ruta):
-    lista=[]
+    print("------ Cambiando el size de las imagenes")
+    listaImagenes=[]
     for x in directorio:
-        im = Image.open(ruta+"\\"+x,"r")
-        newsize = (10, 10)
-        im1 = im.resize(newsize)
-        lista.append(im1)
-    print(directorio)
-    return valorRGB(lista)
+        im = Image.open(os.path.join(ruta, x),"r")
+        newsize = (15, 15)
+        im1 = {'imagen':im.resize(newsize), 'nombre':x, 'promRGB':0}
 
-#
-#
+        listaImagenes.append(im1)
+    return valorRGB(listaImagenes)
+
+# Calcula el valor promedio RGB de cada imagen 
+#E lista: diccionario Lista de diccionario con información de cada imágen 
 #
 def valorRGB(lista):
-    print("test")
+    print("------ Calculando el Promedio RGB")
 
     for x in lista:
-        arr = np.array(x)
+        arr = np.array(x['imagen']) # Vamos a convertir la imagen en un arreglo
+        
         arr_mean = np.mean(arr, axis=(0,1))
 
-        print(arr_mean)
-        
-        #print(f'[R={arr_mean[0]:.1f},  G={arr_mean[1]:.1f}, B={arr_mean[2]:.1f} ]')
-
-#C:\Users\Usuario\Desktop\8 semestre\Arquitectura computadores\Proyecto 1\Fotomosaico\PROYECTO1_ARQUI_MULTIPROSSESING\imgs
-
-
-
-
-
-
-
-
+        if(arr.ndim != 3):
+            nombreImagen = x['nombre']
+            print(f'Imagen {nombreImagen} tiene {arr.ndim} dimensiones, el shape es: {arr.shape}, el promedio RGB es de {arr_mean}')
+            #Muestra la Imagen 
+            # plt.imshow(x['imagen'])
+            # plt.show()
+            x['imagen'].show()
+        else:
+            print(f'[R={arr_mean[0]:.1f},  G={arr_mean[1]:.1f}, B={arr_mean[2]:.1f} ]')
 
 
 def valorPromedioImg():
