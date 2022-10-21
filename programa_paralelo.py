f6f5c309e06b63b4a758b31da037133cbffba897
ValenTiss <100385088+ValenTiss@users.noreply.github.com>
2022-10-21 00:47:50 -0400

    ValorRGBParalelizado(FINISHED)

--- programa_paralelo.py
+++ programa_paralelo.py
@@ -86,11 +86,87 @@ def cambioTamanioImg(directorio,ruta):
     
     return listaImagenes
 
+diccionarioImagenes = {}
+diccionarioPromedios = {}
+
+
+def waitSec(x):
+    time.sleep(0.0001)
+    return x 
+
+
+@ray.remote
+def obtenerRGB(x):
+    arr = np.array(x['imagen']) # Vamos a convertir la imagen en un arreglo
+        
+    arr_mean = np.mean(arr, axis=(0,1))
+
+    if(arr.ndim != 3):
+        nombreImagen = x['nombre']
+        #print(f'Imagen {nombreImagen} tiene {arr.ndim} dimensiones, el shape es: {arr.shape}, el promedio RGB es de {arr_mean}')
+
+        # {'128,250,74':<imagen>, '123,345,432':<imagen2>, }
+
+        # {'128,250,74':<imagen>, '123,345,432':<imagen2>, }
+        # [12825074, 123345432, 886878687, 98168761847]
+
+        rgb = waitSec(f'{int(arr_mean)}{int(arr_mean)}{int(arr_mean)}')
+
+        "255255255"
+        
+        listaRGB = waitSec([arr_mean,arr_mean,arr_mean])
+
+        diccionarioImagenes[rgb] =  waitSec(x['imagen'])
+        diccionarioPromedios[rgb] =   waitSec(listaRGB)
+
+        #Muestra la Imagen 
+        # plt.imshow(x['imagen'])
+        # plt.show()
+        #x['imagen'].show()
+    else:
+
+        # print(f'[R={int(arr_mean[0])},  G={int(arr_mean[1])}, B={int(arr_mean[2])} ]')
+        # El valor RGB
+        
+
+        rgb = waitSec(f'{int(arr_mean[0])}{int(arr_mean[1])}{int(arr_mean[2])}')
+
+        listaRGB = waitSec([int(arr_mean[0]), int(arr_mean[1]),int(arr_mean[2])])
+
+        diccionarioPromedios[rgb] = waitSec(listaRGB)
+        diccionarioImagenes[rgb] = waitSec(x['imagen'])
+
+    
+def valorRGBParalelizado(lista):
+    ray.get([obtenerRGB.remote(waitSec(x)) for x in lista])
+
+
+def menuSeleccionImagenBase( listaImagenes ):
+    print("*****Seleccione la Imagen a procesar******\n")
+
+    for iterador, imagen in enumerate(listaImagenes):
+        print(f'Opcion {iterador + 1}: {imagen}')
+
+    print(" ")        
+    try:
+        opcion=int(input("Digite su opcion:"))
+    except:
+        print("Opción no válida\n")
+        return menuSeleccionImagenBase(listaImagenes)
+
+    if ( opcion > 0 and opcion <= len(listaImagenes)):
+        print(f'Imagen {opcion} seleccionada')
+        return (listaImagenes[opcion - 1 ])
+    else:
+        print("Opción no válida")
+        return menuSeleccionImagenBase(listaImagenes)
+   
 
 def tiempo():
   inicio = time.time()
   directorioImagenes = cargaImagenes(pathImagenes)
-  = cambioTamanioImg(directorioImagenes, pathImagenes)
+  imagenesModificadas = cambioTamanioImg(directorioImagenes, pathImagenes)
+  diccionarioRGB = valorRGBParalelizado(imagenesModificadas)
   print("Duración: ", time.time() - inicio)
 
 tiempo()
